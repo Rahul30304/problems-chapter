591cbdd701cb38ab49c5d60b034fbe42dd2da0fb
Rahul30304 <rahulram30304@gmail.com>
2024-12-05 11:24:48 +0530

    Add files via upload

---
+++ Pattern 7/Problem1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Problem1 {
+    public static void main(String[] args) {
+        System.out.println("Enter input");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=n-i;j>=1;j--){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        
+    }
+}
